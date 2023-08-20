56b3b0f8a703f579ecab0c9d638e37b179b52a3d
Santiago-Soto-21 <santisotofajardo@gmail.com>
2023-08-20 19:48:08 -0400

    Changed variables for better structure

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.render("Courriel");
+  res.render("index");
 });
 
 const __filename = fileURLToPath(import.meta.url);
@@ -43,16 +43,13 @@ app.post("/addKeys", (req, res) => {
 });
 
 app.post("/addLetters", (req, res) => {
-  //console.log(req);
-  console.log(req.body);
-
-  // Read the content inside "Tableau1.json"
+  // Read the content inside "outbox.json"
   const fileContents = fs.readFileSync("outbox.json", "utf-8");
 
   // Parse the contents to a JavaScript object
   const messagesData = JSON.parse(fileContents);
 
-  // Add the encrypted object to the "Tableau" array in the JavaScript object
+  // Add the encrypted object to the "outbox" array in the JavaScript object
   messagesData.unshift(req.body);
 
   // Convert the updated JavaScript object back to JSON string
@@ -62,7 +59,7 @@ app.post("/addLetters", (req, res) => {
   fs.writeFileSync("outbox.json", updatedFileContents);
 
   // Send a success response
-  res.status(201).send("Le message a été envoyé");
+  res.status(201).send("The email was sent");
 });
 
 app.get("/mergeLetters", (req, res) => {
@@ -72,7 +69,7 @@ app.get("/mergeLetters", (req, res) => {
   // Parse the contents to a JavaScript object
   const messagesData = JSON.parse(fileContents);
 
-  // Send the "Tableau" array from the JavaScript object as a JSON response
+  // Send the "outbox" array from the JavaScript object as a JSON response
   res.json(messagesData);
 })
 
@@ -104,19 +101,19 @@ app.get("/getPrivateKey", (req, res) => {
   }
 });
 
-const paires = ["http://127.0.0.1:3001", "http://127.0.0.1:3000"];
+const peers = ["http://127.0.0.1:3001", "http://127.0.0.1:3000"];
 var URL = "http://127.0.0.1:3000";
 
 const getReceiverPublicKey = (callback) => {
-  var paireActuel = paires[0] + "/getPublicKey";
+  var currentPeer = peers[0] + "/getPublicKey";
 
-  if (paireActuel === URL + "/getPublicKey") {
-    paires.push(paires.shift());
+  if (currentPeer === URL + "/getPublicKey") {
+    peers.push(peers.shift());
     callback(new Error("Receiver not found"), null);
     return;
   }
 
-  request(paireActuel, (err, res, body) => {
+  request(currentPeer, (err, res, body) => {
     if (err) {
       callback(err, null);
     } else {
@@ -152,7 +149,6 @@ app.get("/getLetters", (req, res) => {
 
     // Send the data as a JSON response
     res.json(messagesData);
-    console.log(messagesData);
   } catch (err) {
     console.error(err);
     res.status(500).send("Internal Server Error");
@@ -160,35 +156,34 @@ app.get("/getLetters", (req, res) => {
 });
 
 var refreshInbox = () => {
-  var paireActuel = paires[0] + "/mergeLetters";
-  if (paireActuel == URL + "/mergeLetters") {
-    paires.push(paires.shift());
+  var currentPeer = peers[0] + "/mergeLetters";
+  if (currentPeer == URL + "/mergeLetters") {
+    peers.push(peers.shift());
     return;
   }
 
   let myInbox = JSON.parse(fs.readFileSync("inbox.json", "utf-8"));
 
-  request(paireActuel, (err, res, body) => {
+  request(currentPeer, (err, res, body) => {
     if (err) {
       return console.log(err);
     }
 
-    console.log(body);
     const senderOutbox = JSON.parse(body);
 
-    // Update the array in "Tableau.json" with elements from "tableau2"
+    // Update the array in "inbox.json" with elements from "outbox.json" from currentPeer
     senderOutbox.forEach((item) => {
-      // Check if the item is not already in "tableau1"
+      // Check if the item is not already in "inbox.json"
       const existingItemIndex = myInbox.findIndex((existingItem) => {
         return (
-          existingItem.contact.cipher === item.contact.cipher &&
-          existingItem.titre.cipher === item.titre.cipher &&
-          existingItem.message.cipher === item.message.cipher
+          existingItem.contact === item.contact &&
+          existingItem.title === item.title &&
+          existingItem.message === item.message
         );
       });
 
       if (existingItemIndex === -1) {
-        // If the item is not already in "tableau1", push it to the array
+        // If the item is not already in "inbox.json", push it to the array
         myInbox.unshift(item);
       }
     });
@@ -196,14 +191,11 @@ var refreshInbox = () => {
     // Update the local JSON file with the merged array
     fs.writeFileSync("inbox.json", JSON.stringify(myInbox, null, 2));
 
-    console.log(
-      "Local JSON file updated with merged array:",
-      JSON.stringify(myInbox, null, 2)
-    );
+    console.log("Local JSON file updated with merged array:", JSON.stringify(myInbox, null, 2));
   });
 };
 
 app.listen(3000, () => {
-  console.log("Le serveur est dans le port 3000");
+  console.log("The server is in port 3000");
   setInterval(refreshInbox, 15000);
 });
