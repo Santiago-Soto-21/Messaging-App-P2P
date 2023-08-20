ac3d2b871a48c59791ae45e1cd980e6f4ba77de9
Santiago-Soto-21 <santisotofajardo@gmail.com>
2023-08-20 19:48:55 -0400

    Fixed bug with New Mail button

--- public/js/script.js
+++ public/js/script.js
@@ -1,147 +1,172 @@
-// Ouvrir les fichiers suivants: Server1, Server2, Tableau1, Tableau2, Testgenerator et TestEncrypt.
+// Initializing global variables
 const forge = window.forge;
 let sendKeysExecuted = false;
-
-//Partie projet 1
-var optionValue = "Boite de réception"; //valeur initial du drop menu
+var optionValue = "Inbox";
 
 Drop_menu(); //S'assurer que la liste soit dans le bon mode au lancement de la page
-//Fonction qui permet la recherche de nom ou d'adresse
+//Fonction qui permet la search de nom ou d'address
 
-function recherche() {
+function search() {
   let a, str, div;
-  let Entree = document.getElementById("recherche");
-  let Filtre = Entree.value.toUpperCase();
-  let Envoyes = document.getElementById("Envoyes");
-  let Recu = document.getElementById("Recu");
-  if (optionValue == "Emails envoyés") {
-    div = Envoyes.getElementsByTagName("div");
+  let Input = document.getElementById("search");
+  let Filter = Input.value.toUpperCase();
+  let Sent = document.getElementById("Sent");
+  let Received = document.getElementById("Received");
+  if (optionValue == "Emails sent") {
+    div = Sent.getElementsByTagName("div");
   }
-  if (optionValue == "Boite de réception") {
-    div = Recu.getElementsByTagName("div");
+  if (optionValue == "Inbox") {
+    div = Received.getElementsByTagName("div");
   }
 
   for (let i = 0; i < div.length; i++) {
     a = div[i].getElementsByTagName("a")[0];
     str = a.textContent || a.innerText;
-    if (str.toUpperCase().indexOf(Filtre) > -1) {
+    if (str.toUpperCase().indexOf(Filter) > -1) {
       div[i].style.display = "";
     } else {
       div[i].style.display = "none";
     }
   }
 }
-// Fonction qui affiche le formulaire d'un nouveau message
+// Fonction qui affiche le form d'un nouveau message
 function NewMail() {
-  document.getElementById("formulaire").style.display = "block";
+  deleteMail();
+
+  document.getElementById("form").style.display = "block";
+
+  // Create "Send" button
+  var sendButton = document.createElement("button");
+  sendButton.style.marginLeft = "5px";
+  sendButton.style.height = "27px";
+  sendButton.textContent = "Send ➡️";
+  sendButton.onclick = addMail;
+
+  // Create "Cancel" button
+  var cancelButton = document.createElement("button");
+  cancelButton.style.height = "27px";
+  cancelButton.style.marginTop = "8px";
+  cancelButton.textContent = "Cancel ❌";
+  cancelButton.onclick = deleteMail;
+
+  // Add buttons to the form
+  var form = document.getElementById("form");
+  form.appendChild(sendButton);
+  form.appendChild(cancelButton);
 }
 
 // Fonction qui ajoute un courriel à la liste d'envois lorsque "Envoyer" est appuyé
 function addMail() {
-  const title = document.createElement("div");
+  const titled = document.createElement("div");
   const titlename = document.createTextNode(
-    document.getElementById("Titre").value
+    document.getElementById("Title").value
   );
   const a = document.createElement("a");
   const br = document.createElement("br");
   const p = document.createElement("p");
   const useremail = document.createTextNode(
-    document.getElementById("Adresse").value
+    document.getElementById("Address").value
   );
   const content = document.createTextNode(
-    document.getElementById("Contenu").value
+    document.getElementById("Content").value
   );
-  title.appendChild(a);
+  titled.appendChild(a);
   a.appendChild(useremail);
   a.appendChild(br);
   a.appendChild(titlename);
-  title.appendChild(p);
+  titled.appendChild(p);
   p.appendChild(content);
-  document.getElementById("Envoyes").appendChild(title);
-  title.setAttribute("class", "Carte");
-  title.setAttribute("onclick", "apercu_email(this.id)");
+  document.getElementById("Sent").appendChild(titled);
+  titled.setAttribute("class", "Card");
+  titled.setAttribute("onclick", "emailForm(this.id)");
   let time = new Date().getTime();
-  title.setAttribute("id", time);
+  titled.setAttribute("id", time);
   p.setAttribute("style", "display:none;");
 
-  const contact = document.getElementById("Adresse").value;
-  const titre = document.getElementById("Titre").value;
-  const message = document.getElementById("Contenu").value;
-
-  console.log(contact);
+  const contact = document.getElementById("Address").value;
+  const title = document.getElementById("Title").value;
+  const message = document.getElementById("Content").value;
 
   const email = {
     contact: contact,
-    titre: titre,
+    title: title,
     message: message,
-  }
+  };
 
   sendMessage(email);
 
-  document.getElementById("Titre").value = "";
-  document.getElementById("Adresse").value = "";
-  document.getElementById("Contenu").value = "";
-  document.getElementById("formulaire").style.display = "none";
+  document.getElementById("Title").value = "";
+  document.getElementById("Address").value = "";
+  document.getElementById("Content").value = "";
+  document.getElementById("form").style.display = "none";
 }
 
 // Fonction qui efface le courriel en cours lorsque "Abandonner" est appuyé
 function deleteMail() {
-  document.getElementById("Titre").value = "";
-  document.getElementById("Adresse").value = "";
-  document.getElementById("Contenu").value = "";
-  document.getElementById("formulaire").style.display = "none";
+  document.getElementById("Title").value = "";
+  document.getElementById("Address").value = "";
+  document.getElementById("Content").value = "";
+
+  var form = document.getElementById("form");
+  var existingButtons = form.querySelectorAll("button");
+
+  existingButtons.forEach(button => {
+    button.remove();
+  });
+
+  document.getElementById("form").style.display = "none";
 }
 
-// Fonction qui affiche le contenu du courriel
-function apercu_email(clickedId) {
-  document.getElementById("formulaire").style.display = "block";
-  document.getElementById("Titre").value = document
+// Fonction qui affiche le content du courriel
+function emailForm(clickedId) {
+  document.getElementById("form").style.display = "block";
+  document.getElementById("Title").value = document
     .getElementById(clickedId)
     .getElementsByTagName("a")[0].childNodes[2].textContent;
-  document.getElementById("Adresse").value = document
+  document.getElementById("Address").value = document
     .getElementById(clickedId)
     .getElementsByTagName("a")[0].childNodes[0].textContent;
-  document.getElementById("Contenu").value = document
+  document.getElementById("Content").value = document
     .getElementById(clickedId)
     .getElementsByTagName("p")[0].innerText;
 }
 
-function messageRecu(contact, titre, message) {
-  document.getElementById("formulaire").style.display = "block";
-  document.getElementById("Titre").value = titre;
-  document.getElementById("Adresse").value = contact;
-  document.getElementById("Contenu").value = message;
+function messageReceived(contact, title, message) {
+  document.getElementById("form").style.display = "block";
+  document.getElementById("Title").value = title;
+  document.getElementById("Address").value = contact;
+  document.getElementById("Content").value = message;
 }
 
-//Fonction du drop menu qui affiche la boite de reception, le carnet d'adresse ou
+//Fonction du drop menu qui affiche la boite de reception, le carnet d'address ou
 function Drop_menu() {
   var optionValue = document.getElementById("Option_menu").value; //valeur selectioné dans le drop menu
 
   //Affichage Boite de reception
-  if (optionValue == "Boite de réception") {
-    document.getElementById("Envoyes").style.display = "none";
-    document.getElementById("Recu").style.display = "block";
+  if (optionValue == "Inbox") {
+    document.getElementById("Sent").style.display = "none";
+    document.getElementById("Received").style.display = "block";
 
     getMessages();
     return optionValue;
   }
 
-  //Affichage emails envoyés
-  if (optionValue == "Emails envoyés") {
-    document.getElementById("Envoyes").style.display = "block";
-    document.getElementById("Recu").style.display = "none";
+  //Affichage emails sent
+  if (optionValue == "Emails sent") {
+    document.getElementById("Sent").style.display = "block";
+    document.getElementById("Received").style.display = "none";
 
     return optionValue;
   }
 }
 
 //////////////////////////////////////////////////////////////////////////////
-//Partie projet 2
+// Fetch and get requests
 
 // Generate RSA key pair for encryption and decryption
 
-async function sendKeys(){
-  try{
+async function sendKeys() {
+  try {
     const keyPair = forge.pki.rsa.generateKeyPair({ bits: 1024 });
     const privateKeyPem = forge.pki.privateKeyToPem(keyPair.privateKey);
     const publicKeyPem = forge.pki.publicKeyToPem(keyPair.publicKey);
@@ -156,16 +181,16 @@ async function sendKeys(){
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(keys), 
+      body: JSON.stringify(keys, null, 2),
     });
 
-    if(response.ok){
+    if (response.ok) {
       console.log("Keys were sent.");
       sendKeysExecuted = true;
-    } else{
+    } else {
       console.error("Could not send keys:", response.statusText);
     }
-  }catch(error){
+  } catch (error) {
     console.error("Could not send keys:", error);
   }
 }
@@ -173,27 +198,23 @@ async function sendKeys(){
 var receiverPublicKey = null;
 var privateKey = null;
 
-async function getReceiverPublicKey(){
-  try{
+async function getReceiverPublicKey() {
+  try {
     const response = await fetch("/getReceiverPublicKey", {
       method: "GET",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/text",
       },
     });
-    if(response.ok){
+    if (response.ok) {
       const receiverPublicKeyPem = await response.text();
-      
+
       // Convert PEM keys to Forge objects
       receiverPublicKey = forge.pki.publicKeyFromPem(receiverPublicKeyPem);
-      
-      console.log("Receiver public Key:", receiverPublicKeyPem);
-      console.log("Public key:", receiverPublicKey);
-
-    } else{
+    } else {
       console.error("Could not get private key:", response.statusText);
     }
-  } catch(error){
+  } catch (error) {
     console.error("Could not get private key:", error);
   }
 }
@@ -217,44 +238,43 @@ async function sendMessage(email) {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(encryptedData), // Convert the message object to JSON string and include it as the request body
+      body: JSON.stringify(encryptedData, null, 2), // Convert the message object to JSON string and include it as the request body
     });
 
     // Check the response status
     if (response.ok) {
       // Request was successful
-      console.log("Le message a été envoyé avec succès");
+      console.log("The email was successfully sent");
     } else {
       // Request failed
-      console.error("Le message n'a pas pu être envoyé:", response.statusText);
+      console.error("Unable to send email:", response.statusText);
     }
   } catch (error) {
-    console.error("Le message n'a pas pu être envoyé:", error);
+    console.error("Unable to send email:", error);
   }
 }
 
-async function getPrivateKey(){
-  try{
-    if(!sendKeysExecuted){
+async function getPrivateKey() {
+  try {
+    if (!sendKeysExecuted) {
       await sendKeys();
     }
 
     const response = await fetch("/getPrivateKey", {
       method: "GET",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/text",
       },
     });
-    if(response.ok){
+    if (response.ok) {
       const privateKeyPem = await response.text();
 
       // Convert PEM string to Forge private key object
       privateKey = forge.pki.privateKeyFromPem(privateKeyPem);
-
-    } else{
+    } else {
       console.error("Could not get private key:", response.statusText);
     }
-  } catch(error){
+  } catch (error) {
     console.error("Could not get private key:", error);
   }
 }
@@ -283,92 +303,87 @@ async function getMessages() {
       // Response received successfully
       const messagesData = await response.json(); // Parse the response as JSON
 
-      // Access the "Tableau" array from the JSON data
-      const tableauArray = decryption(privateKey, messagesData);
+      // Access the "Inbox" array from the JSON data
+      const inboxArray = decryption(privateKey, messagesData);
 
       // Access the element in the DOM where you want to display the JSON data
-      const outputElement = document.getElementById("Recu");
+      const outputElement = document.getElementById("Received");
 
       // Clear any existing content in the outputElement
       outputElement.innerHTML = "";
 
       // Loop through the array and create HTML elements to display the data
-      tableauArray.forEach((item) => {
+      inboxArray.forEach((item) => {
         // Create container div for each message
-        if(item.contact && item.titre && item.message != undefined){
-        const messageContainer = document.createElement("div");
-        messageContainer.setAttribute("class", "Carte");
-        messageContainer.setAttribute(
-          "onclick",
-          `messageRecu('${item.contact}', '${item.titre}', '${item.message}')`
-        );
+        if (item.contact && item.title && item.message != undefined) {
+          const messageContainer = document.createElement("div");
+          messageContainer.setAttribute("class", "Card");
+          messageContainer.setAttribute(
+            "onclick",
+            `messageReceived('${item.contact}', '${item.title}', '${item.message}')`
+          );
 
-        // Create contact element
-        const a = document.createElement("a");
-        const br = document.createElement("br");
-        const p = document.createElement("p");
+          // Create contact element
+          const a = document.createElement("a");
+          const br = document.createElement("br");
+          const p = document.createElement("p");
 
-        const contact = document.createTextNode(item.contact);
-        const titre = document.createTextNode(item.titre);
-        const message = document.createTextNode(item.message);
+          const contact = document.createTextNode(item.contact);
+          const title = document.createTextNode(item.title);
+          const message = document.createTextNode(item.message);
 
-        messageContainer.appendChild(a);
-        a.appendChild(contact);
-        a.appendChild(br);
-        a.appendChild(titre);
-        messageContainer.appendChild(p);
-        p.appendChild(message);
-        p.setAttribute("style", "display:none;");
+          messageContainer.appendChild(a);
+          a.appendChild(contact);
+          a.appendChild(br);
+          a.appendChild(title);
+          messageContainer.appendChild(p);
+          p.appendChild(message);
+          p.setAttribute("style", "display:none;");
 
-        // Append the message container to the outputElement
-        outputElement.appendChild(messageContainer);
-       }
+          // Append the message container to the outputElement
+          outputElement.appendChild(messageContainer);
+        }
       });
     } else {
       // Request failed
-      console.error(
-        "Les messages n'ont pas pu être reçus:",
-        response.statusText
-      );
+      console.error("Could not receive emails:", response.statusText);
     }
   } catch (error) {
-    console.error("Les messages n'ont pas pu être reçus:", error);
+    console.error("Could not receive emails:", error);
   }
 }
 
 const encryption = (receiverPublicKey, email) => {
   // Create variables that contain the properties that need to be encrypted
   const contact = email.contact;
-  const titre = email.titre;
+  const title = email.title;
   const message = email.message;
-
-  console.log("Adresse: ", contact);
 
   // Convert input data to binary using UTF-8 encoding
   const contactBytes = forge.util.encodeUtf8(contact);
-  const titreBytes = forge.util.encodeUtf8(titre);
+  const titleBytes = forge.util.encodeUtf8(title);
   const messageBytes = forge.util.encodeUtf8(message);
 
   // Encrypt the message properties using the RSA public key
-  const encryptedContact = receiverPublicKey.encrypt(contactBytes, 'RSA-OAEP', {
+  const encryptedContact = receiverPublicKey.encrypt(contactBytes, "RSA-OAEP", {
     md: forge.md.sha256.create(),
   });
-  const encryptedTitre = receiverPublicKey.encrypt(titreBytes, 'RSA-OAEP', {
+  const encryptedTitle = receiverPublicKey.encrypt(titleBytes, "RSA-OAEP", {
     md: forge.md.sha256.create(),
   });
-  const encryptedMessage = receiverPublicKey.encrypt(messageBytes, 'RSA-OAEP', {
+  const encryptedMessage = receiverPublicKey.encrypt(messageBytes, "RSA-OAEP", {
     md: forge.md.sha256.create(),
   });
 
   // Convert the encrypted binary data to Base64 strings
   const encryptedContactBase64 = forge.util.encode64(encryptedContact);
-  const encryptedTitreBase64 = forge.util.encode64(encryptedTitre);
+  const encryptedTitleBase64 = forge.util.encode64(encryptedTitle);
   const encryptedMessageBase64 = forge.util.encode64(encryptedMessage);
 
   // Create a new object with encrypted properties
   const encryptedData = {
     contact: encryptedContactBase64,
-    titre: encryptedTitreBase64,
+    title: encryptedTitleBase64,
     message: encryptedMessageBase64,
   };
 
@@ -379,27 +394,33 @@ const decryption = (privateKey, messagesData) => {
   const decryptedData = messagesData.map((encryptedData) => {
     // Convert Base64 strings back to binary data
     const decodedContactBytes = forge.util.decode64(encryptedData.contact);
-    const decodedTitreBytes = forge.util.decode64(encryptedData.titre);
+    const decodedTitleBytes = forge.util.decode64(encryptedData.title);
     const decodedMessageBytes = forge.util.decode64(encryptedData.message);
 
-    const decryptedContact = privateKey.decrypt(decodedContactBytes, 'RSA-OAEP', {
+    const decryptedContact = privateKey.decrypt(
+      decodedContactBytes,
+      "RSA-OAEP",
+      {
+        md: forge.md.sha256.create(),
+      }
+    );
+    const decryptedTitle = privateKey.decrypt(decodedTitleBytes, "RSA-OAEP", {
       md: forge.md.sha256.create(),
     });
-    const decryptedTitre = privateKey.decrypt(decodedTitreBytes, 'RSA-OAEP', {
-      md: forge.md.sha256.create(),
-    });
-    const decryptedMessage = privateKey.decrypt(decodedMessageBytes, 'RSA-OAEP', {
-      md: forge.md.sha256.create(),
-    });
-
-    console.log(decryptedMessage);
+    const decryptedMessage = privateKey.decrypt(
+      decodedMessageBytes,
+      "RSA-OAEP",
+      {
+        md: forge.md.sha256.create(),
+      }
+    );
 
     return {
       contact: decryptedContact,
-      titre: decryptedTitre,
+      title: decryptedTitle,
       message: decryptedMessage,
     };
-  });  
+  });
 
   return decryptedData;
 };
